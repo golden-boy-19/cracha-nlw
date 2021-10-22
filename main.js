db9c4d407f5405126273a4bf9672f226e4aadf8a
GoldenBoy <72346145+golden-boy-19@users.noreply.github.com>
2021-10-22 19:21:00 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+const LinksSocialMedia = {
+  github: 'golden-boy-19',
+  youtube: 'channel/UCsdRj-XxOv5aNwcFK6U8UIg',
+  facebook: 'gabriel.neris.7',
+  instagram: 'gabrielpandora19',
+  twitter: 'GoldenBlockReal'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+  }
+}
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      bio.textContent = data.bio
+      userLink.href = data.html_url
+      userImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
